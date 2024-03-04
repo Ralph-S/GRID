48fe5513427d740700c285c9df60593b42432b7e
Ralph Sakhat <ralph.sakhat@lau.edu>
2024-03-05 01:19:53 +0200

    Updating installation file to install node

--- installation.bash
+++ installation.bash
@@ -1,28 +1,6 @@
-#!/bin/bash
-cd
-cd user-interface/LocalApp/
-# source venv/bin/activate
+cd 
 
-pip install --upgrade pip setuptools
-
-pip3 install flask
-pip3 install flask-wtf
-# pip3 install pyyaml
-# pip3 install rospkg
-# pip3 install rospy
-# pip3 install flask-socketio
-# pip3 install statistics
-# pip3 install mongoengine
-# pip3 install paho-mqtt
-# pip3 install numpy
-# pip3 install gevent
-# pip3 install ruamel.yaml
-# pip3 install gevent-websocket==0.10.1
-# sudo apt-get install -y xdotool
-# sudo apt install python3-gevent
-# sudo apt-get install -y python-jinja2
-
-
-# pip3 install flask-socketio==4.3.0
-# pip3 install python-engineio==3.13.0
-# pip3 install python-socketio==4.6.0
+sudo apt update && sudo apt upgrade -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install node
