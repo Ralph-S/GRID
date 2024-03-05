f1ed34736a42b6c9a34fe0f048958f5d58d2ab27
Ralph Sakhat <ralph.sakhat@lau.edu>
2024-03-05 02:07:15 +0200

    Creating UI branch and startup for react

---
+++ startup.bash
@@ -0,0 +1,8 @@
+#If this is your first time running the project on your machine, uncomment the two lines below. 
+#From the second time onwards, keep them commented
+
+# bash installation.bash
+# npx create-react-app app
+
+cd app
+npm start
