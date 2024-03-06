34a5f79fe2daa24ce95d24fd6f78a666a5a5325b
Ralph Sakhat <ralph.sakhat@lau.edu>
2024-03-06 07:26:04 +0200

    Creating route for Emergency Override and logging entry

--- app/src/app.py
+++ app/src/app.py
@@ -2,18 +2,11 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)
 
-@app.route('/toggle-cam', methods=['POST'])
-def toggle_cam():
-    # This is where you handle the toggle
-    # For now, let's just return a simple JSON
-    return jsonify({"status": "CAM toggled"})
-
-@app.route('/toggle-thermal', methods=['POST'])
-def toggle_thermal():
-    # Handle the thermal toggle here
-    return jsonify({"status": "Thermal toggled"})
+@app.route('/emergency-override', methods=['POST'])
+def emergency_override():
+    return jsonify({"status": "Emergency Mode Activated"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/src/components/EmergencyOverride.js
+++ app/src/components/EmergencyOverride.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 const EmergencyOverride = () => {
+  const handleEmergency = () => {
+    fetch('http://localhost:5000/emergency-override', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({emergency: true}),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+
   return (
     <div className="component emergency-override">
-      <button className='round-button'>
+      <button className='round-button' onClick={handleEmergency}>
         <div>Emergency</div>
         <div>Override</div>
       </button>
