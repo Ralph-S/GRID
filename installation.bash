45a12100a5b09d61c512e8fec1b9ade2f7463f6c
Ralph Sakhat <ralph.sakhat@lau.edu>
2024-03-07 20:29:32 +0200

    Setting up DM branch

--- installation.bash
+++ installation.bash
@@ -2,9 +2,13 @@ cd
 
 sudo apt update && sudo apt upgrade -y
 
+#UI installations
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install node
 
 pip install Flask
 pip install Flask-Cors
+
+#DM installations
